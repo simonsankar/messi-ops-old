7c3a5f233819aa4a6e338dfec5a8bef149f10462
Simon Sankar <sjsankar10@gmail.com>
2018-12-03 15:40:55 -0400

    Responsive header for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Menu, Responsive } from "semantic-ui-react";
+
 class Home extends Component {
+  state = { activeItem: "home" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
   render() {
+    const { activeItem } = this.state;
     return (
       <div className="ops">
         <Container>
@@ -15,12 +20,51 @@ class Home extends Component {
                 tablet={8}
                 mobile={16}
               >
-                <div className="ops-header">
-                  <div className="ops-messi">"Messi"</div>
-                  <div className="ops-ops">Ops"</div>
-                </div>
+                <Responsive minWidth={Responsive.onlyMobile.maxWidth}>
+                  <div className="ops-header">
+                    <div className="ops-messi">"Messi"</div>
+                    <div className="ops-ops">Ops"</div>
+                  </div>
+                </Responsive>
+                <Responsive maxWidth={Responsive.onlyTablet.minWidth}>
+                  <div className="ops-header">
+                    <div className="ops-messi ops-messi-mobile">"Messi"</div>
+                    <div className="ops-ops ops-ops-mobile">Ops"</div>
+                  </div>
+                </Responsive>
               </Grid.Column>
-              <Grid.Column computer={8} tablet={8} mobile={16} />
+              <Grid.Column
+                computer={8}
+                tablet={8}
+                mobile={16}
+                textAlign="right"
+                verticalAlign="middle"
+              >
+                <Menu
+                  className="side-menu"
+                  pointing
+                  secondary
+                  vertical
+                  floated="right"
+                >
+                  <Menu.Item
+                    className="side-item"
+                    name="home"
+                    active={activeItem === "home"}
+                    onClick={this.handleItemClick}
+                  />
+                  <Menu.Item
+                    name="messages"
+                    active={activeItem === "messages"}
+                    onClick={this.handleItemClick}
+                  />
+                  <Menu.Item
+                    name="friends"
+                    active={activeItem === "friends"}
+                    onClick={this.handleItemClick}
+                  />
+                </Menu>
+              </Grid.Column>
             </Grid.Row>
           </Grid>
         </Container>
