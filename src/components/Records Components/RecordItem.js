3c1991da5646b3a1658a9d45e3f0791aada45a9d
Simon Sankar <sjsankar10@gmail.com>
2019-01-03 14:54:36 -0400

    Saving

--- src/components/Records Components/RecordItem.js
+++ src/components/Records Components/RecordItem.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+
+import { Grid, Image } from "semantic-ui-react";
 
 export const RecordItem = () => {
-  return <div className="record-item">rec</div>;
+  return (
+    <div className="record-item">
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Image
+              className="record-img"
+              circular
+              size="small"
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt265gDR-meyzXdUbLBQIYoP6-EcIx0DpCizAO1jgcxW2Yi_hEgw"
+              }
+            />
+          </Grid.Column>
+          <Grid.Column width={10} />
+        </Grid.Row>
+        <Grid.Row />
+      </Grid>
+    </div>
+  );
 };
 export const RecordItemAlt = () => {
   return <div className="record-item-alt">rec</div>;
