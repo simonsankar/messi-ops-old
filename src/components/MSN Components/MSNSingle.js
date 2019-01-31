b54a60b7cc8636d40764ef886c2683b81d46f351
Simon Sankar <sjsankar10@gmail.com>
2019-01-31 16:13:36 -0400

    Saverini

--- src/components/MSN Components/MSNSingle.js
+++ src/components/MSN Components/MSNSingle.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { Grid, Image, Responsive, List } from "semantic-ui-react";
+import HomeCareer from "../Home Components/HomeCareer";
 import LM from "../../images/Messi.png";
-import NJ from "../../images/Neymar.png";
 import LS from "../../images/Suarez.png";
+import NJ from "../../images/Neymar.png";
 
 const MSNSingle = ({ img }) => {
   return (
@@ -17,18 +18,25 @@ const MSNSingle = ({ img }) => {
             verticalAlign="middle"
           >
             <Image
-              className="msn-image"
+              className="msn-single-img"
               size="small"
-              src={img === 1 ? LM : img === 2 ? NJ : LS}
+              src={img === 1 ? LM : img === 2 ? LS : NJ}
             />
           </Responsive>
           <Grid.Column
-            computer={10}
+            computer={7}
             tablet={12}
             mobile={16}
             className="msn-single-details"
           >
-            <h3 className="msn-single-title">Title of the record</h3>
+            <h3 className="msn-single-title">
+              {img === 1
+                ? "Lionel Messi"
+                : img === 2
+                ? "Luis Suarez"
+                : "Neymar Jr."}
+              <small>{img === 1 ? " 10" : img === 2 ? " 9" : " 11"}</small>
+            </h3>
             <p className="msn-single-desc">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
               commodo ligula eget dolor. Aenean massa. Cum sociis natoque
@@ -41,51 +49,20 @@ const MSNSingle = ({ img }) => {
               semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
             </p>
           </Grid.Column>
-          <Grid.Column computer={3} tablet={4} mobile={16}>
-            <h4 className="msn-single-subtitle">
-              Most goals in a Calendar Year
-            </h4>
-            <List divided relaxed as="ol" inverted size="large">
-              <List.Item as="li">
-                <List.Content>
-                  <List.Header className="record-list-first">
-                    Lionel Messi ♔<span className="record-list-value">91</span>
-                  </List.Header>
-                </List.Content>
-              </List.Item>
-              <List.Item as="li">
-                <List.Content>
-                  <List.Header>
-                    Gerd Muller
-                    <span className="record-list-value">85</span>
-                  </List.Header>
-                </List.Content>
-              </List.Item>
-              <List.Item as="li">
-                <List.Content>
-                  <List.Header>
-                    Pele
-                    <span className="record-list-value">75</span>
-                  </List.Header>
-                </List.Content>
-              </List.Item>
-              <List.Item as="li">
-                <List.Content>
-                  <List.Header>
-                    Cristiano Ronaldo
-                    <span className="record-list-value">69</span>
-                  </List.Header>
-                </List.Content>
-              </List.Item>
-              <List.Item as="li">
-                <List.Content>
-                  <List.Header>
-                    Ferenc Deak
-                    <span className="record-list-value">66</span>
-                  </List.Header>
-                </List.Content>
-              </List.Item>
-            </List>
+          <Grid.Column
+            computer={6}
+            tablet={4}
+            mobile={16}
+            verticalAlign="middle"
+          >
+            <HomeCareer
+              stats={{
+                title: "Contribution",
+                apps: 105,
+                goals: 106,
+                assists: 43
+              }}
+            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
