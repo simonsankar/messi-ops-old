ac9825e23198c496f9822a740b72456be315f8dd
Simon Sankar <sjsankar10@gmail.com>
2019-02-07 15:09:34 -0400

    Random blog types completed

--- src/components/Blog Components/BlogBody.js
+++ src/components/Blog Components/BlogBody.js
@@ -1,39 +1,65 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { BlogAlpha, BlogOmega, BlogDelta } from './BlogItems';
+import React, { Component } from "react";
+import _ from "lodash";
+
+import { Grid } from "semantic-ui-react";
+import { BlogAlpha, BlogOmega, BlogDelta } from "./BlogItems";
+
+const blogs = [
+  {
+    title: "Most Complete Player in the Attacking 3rd",
+    date: "04/02/19",
+    img:
+      "https://gq-images.condecdn.net/image/wzkxZ5zwLK1/crop/1620/f/lionel-messi-gq-14feb18_getty_b.jpg",
+    desc:
+      "Lorem ipsum crazy nothingness I dont know what else to do but type random crap"
+  },
+  {
+    title: "Most Complete Player in the Attacking 3rd",
+    date: "04/02/19",
+    img:
+      "https://gq-images.condecdn.net/image/wzkxZ5zwLK1/crop/1620/f/lionel-messi-gq-14feb18_getty_b.jpg",
+    desc:
+      "Lorem ipsum crazy nothingness I dont know what else to do but type random crap"
+  },
+  {
+    title: "Most Complete Player in the Attacking 3rd",
+    date: "04/02/19",
+    img:
+      "https://gq-images.condecdn.net/image/wzkxZ5zwLK1/crop/1620/f/lionel-messi-gq-14feb18_getty_b.jpg",
+    desc:
+      " Something special Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quod voluptate molestiae adipisci. Dolorum culpa perferendisea tempora. Vitae autem tempora magnam similique odio soluta doloribus enim placeat temporibus neque!"
+  },
+  {
+    title: "Most Complete Player in the Attacking 3rd",
+    date: "04/02/19",
+    img:
+      "https://gq-images.condecdn.net/image/wzkxZ5zwLK1/crop/1620/f/lionel-messi-gq-14feb18_getty_b.jpg",
+    desc:
+      "Lorem ipsum crazy nothingness I dont know what else to do but type random crap"
+  }
+];
+
 export default class BlogBody extends Component {
+  renderBlogItems() {
+    return blogs.map((el, index) => {
+      let ran = _.random(1, 3);
+      return (
+        <Grid.Column stretched computer={4} tablet={5} mobile={8}>
+          {ran === 1 ? (
+            <BlogAlpha blog={el} />
+          ) : ran === 2 ? (
+            <BlogDelta blog={el} />
+          ) : (
+            <BlogOmega blog={el} />
+          )}
+        </Grid.Column>
+      );
+    });
+  }
   render() {
     return (
       <div className="blog-body">
-        <Grid container>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogAlpha />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogOmega />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogOmega />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogAlpha />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogOmega />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogDelta />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogAlpha />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogDelta />
-          </Grid.Column>
-          <Grid.Column stretched computer={4} tablet={5} mobile={8}>
-            <BlogOmega />
-          </Grid.Column>
-        </Grid>
+        <Grid container>{this.renderBlogItems()}</Grid>
       </div>
     );
   }
--- src/components/Blog Components/BlogItems.js
+++ src/components/Blog Components/BlogItems.js
@@ -1,27 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { Card, Image } from 'semantic-ui-react';
-import { gray } from 'ansi-colors';
+import { Card, Image } from "semantic-ui-react";
 
 export const BlogAlpha = ({ blog }) => {
   return (
     <Card fluid className="blog-item">
       <div className="blog-item-img-container">
-        <Image
-          className="blog-item-img"
-          src="https://i.ytimg.com/vi/VXOeZ6TITSY/maxresdefault.jpg"
-        />
+        <Image className="blog-item-img" src={blog.img} />
       </div>
       <Card.Content>
-        <Card.Header className="blog-item-header">
-          A very long and unecessary title
-        </Card.Header>
+        <Card.Header className="blog-item-header">{blog.title}</Card.Header>
         <Card.Meta>
-          <span className="date">05/02/18</span>
+          <span className="date">{blog.date}</span>
         </Card.Meta>
         <Card.Description className="blog-item-desc">
-          A look at the MSN's second season and how solid each were
-          individually.
+          {blog.desc}
         </Card.Description>
       </Card.Content>
     </Card>
@@ -32,11 +25,13 @@ export const BlogDelta = ({ blog }) => {
   return (
     <Card fluid className="blog-item blog-item-delta">
       <Card.Content>
-        <Card.Header>Medium Title Lorem oper dome avia cal tanai</Card.Header>
+        <Card.Header className="blog-item-header-delta">
+          {blog.title}
+        </Card.Header>
         <Card.Meta>
-          <span className="date">12/02/18</span>
+          <span className="date">{blog.date}</span>
         </Card.Meta>
-        <Card.Description>Some beautiful text for nothing</Card.Description>
+        <Card.Description>{blog.desc}</Card.Description>
       </Card.Content>
       <Card.Content extra>I know more</Card.Content>
     </Card>
@@ -48,24 +43,21 @@ export const BlogOmega = ({ blog }) => {
       fluid
       className="blog-item blog-item-omega"
       style={{
-        backgroundImage: `url('https://gq-images.condecdn.net/image/wzkxZ5zwLK1/crop/1620/f/lionel-messi-gq-14feb18_getty_b.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        filter: 'grayscale(60%)'
+        backgroundImage: `url(${blog.img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        filter: "grayscale(70%)"
       }}
     >
       <Card.Content>
         <Card.Header className="blog-item-header blog-item-header-omega">
-          The title
+          {blog.title}
         </Card.Header>
         <Card.Meta className="blog-item-meta-white">
-          <span className="date">29/02/19</span>
+          <span className="date">{blog.date}</span>
         </Card.Meta>
         <Card.Description className="blog-item-desc-white">
-          Something special Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Quo quod voluptate molestiae adipisci. Dolorum culpa perferendis
-          ea tempora. Vitae autem tempora magnam similique odio soluta doloribus
-          enim placeat temporibus neque!
+          {blog.desc}
         </Card.Description>
       </Card.Content>
     </Card>
